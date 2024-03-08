ac73513c54bf5a4e92157e172bff0a39269eee0e
bitmunu <120228179+bitmunu@users.noreply.github.com>
2024-03-08 11:02:35 +0200

    task 2 - Update index.php

--- code/index.php
+++ code/index.php
@@ -4,3 +4,14 @@ $order = & $very_bad_unclear_name;
 
 $order .= "yes";
 echo"\nMy order is: $very_bad_unclear_name";
+
+$var = 5;
+$var2 = 7;
+$flot = 1.2e4;
+echo"\n{$var}\n{$var2}\n{$flot}\n";
+echo $var+$var2;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "\n";
+echo $last_month - $this_month;
